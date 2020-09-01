c1566c49705ff65e9947323dbe84e110d9bcc5af
Aakash Solanki <solankish66@gmail.com>
2020-09-01 06:56:35 +0530

    Value Failure for Email and Password

--- lib/domain/core/failures.dart
+++ lib/domain/core/failures.dart
@@ -1,1 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'failures.freezed.dart';
+
+@freezed
+abstract class ValueFailure<T> with _$ValueFailure<T> {
+  //? Expected Failure for Email
+  const factory ValueFailure.invalidEmail({
+    @required String failedValue,
+  }) = InvalidEmail<T>;
+
+  //? Expected Failure for Password
+  const factory ValueFailure.shortPassword({
+    @required String failedValue,
+  }) = ShortPassword<T>;
+}
