ace8ab53f3f8c0fc3e9d434bc8f174c2551ea41a
Aakash Solanki <solankish66@gmail.com>
2020-09-01 14:24:58 +0530

    Implemented FireBaseAuth

---
+++ lib/infrastructure/auth/firebase_auth_facade.dart
@@ -0,0 +1,111 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_notes/domain/auth/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:ddd_notes/domain/auth/auth_failure.dart';
+import 'package:ddd_notes/domain/auth/i_auth_facade.dart';
+import 'package:ddd_notes/domain/auth/value_objects.dart';
+
+//? Implementation of IAuthFacade Contract
+class FirebaseAuthFacade implements IAuthFacade {
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  FirebaseAuthFacade(
+    this._firebaseAuth,
+    this._googleSignIn,
+  );
+
+  @override
+  Future<Option<User>> getSignedInUser() {
+    // TODO: implement getSignedInUser
+    throw UnimplementedError();
+  }
+
+  //
+  @override
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  }) async {
+    //? The _firebaseAuth.createUserWithEmailAndPassword() method
+    //? only accepts the String, So we call getOrCrash() method defined in ValueObject
+    //? Which if String value is correct gives out right() else left containing
+    //? UnexpectedValueError
+    final emailAddressStr = emailAddress.getOrCrash();
+    final passwordStr = password.getOrCrash();
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: emailAddressStr,
+        password: passwordStr,
+      );
+      //? If operation is successful program will simply return unit
+      return right(unit);
+    } on PlatformException catch (e) {
+      //? Instead of throwing out exemption we will return AuthFailure base on value
+      //? of exception
+      //? e: exception contains the value caught by [PlatformException]
+      //? e.code compares to predefined value in [FirebaseAuth]
+      if (e.code == 'ERROR_EMAIL_ALREADY_IN_USE') {
+        return left(const AuthFailure.emailAlreadyInUse());
+      } else {
+        return left(const AuthFailure.serverError());
+      }
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  }) async {
+    final emailAddressStr = emailAddress.getOrCrash();
+    final passwordStr = password.getOrCrash();
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: emailAddressStr,
+        password: passwordStr,
+      );
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.code == 'ERROR_WRONG_PASSWORD' ||
+          e.code == 'ERROR_USER_NOT_FOUND') {
+        return left(const AuthFailure.invalidEmailAndPasswordCombination());
+      } else {
+        return left(const AuthFailure.serverError());
+      }
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithGoogle() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return left(const AuthFailure.cancelledByUser());
+      }
+
+      final googleAuthentication = await googleUser.authentication;
+
+      final authCredential = GoogleAuthProvider.getCredential(
+        idToken: googleAuthentication.idToken,
+        accessToken: googleAuthentication.accessToken,
+      );
+
+      await _firebaseAuth.signInWithCredential(authCredential);
+      return right(unit);
+    } on PlatformException catch (_) {
+      return left(const AuthFailure.serverError());
+    }
+  }
+
+
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
