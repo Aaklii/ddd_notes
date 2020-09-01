e32e9802a5dafc382e599194557f61488341bd0d
Aakash Solanki <solankish66@gmail.com>
2020-09-01 07:30:13 +0530

    Email and Password ValueObjects created having self validation

---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_notes/domain/core/failures.dart';
+import 'package:ddd_notes/domain/core/value_object.dart';
+import 'package:ddd_notes/domain/core/value_validators.dart';
+
+//? EmailAddress ValueObject taking String type value
+//? Having self validation property
+class EmailAddress extends ValueObject<String> {
+  //? Either returns failure if email input value is invalid
+  //? If !(RegExp(emailRegex).hasMatch(input)) [value_validators.dart]
+  //? else correct validated value if (RegExp(emailRegex).hasMatch(input))
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    assert(input != null);
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObject<String> {
+  //? Either returns failure if password input value is invalid
+  //? If !(input.length >= 6) [value_validators.dart]
+  //? else correct validated value if (input.length >= 6)
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    assert(input != null);
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+}
