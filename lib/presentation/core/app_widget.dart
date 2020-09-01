e0d5d16018a9c2d51bbc600f098085e507c952b3
Aakash Solanki <solankish66@gmail.com>
2020-09-01 15:40:05 +0530

    SignInPage and SignInFormPage created

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:ddd_notes/application/auth/auth_bloc.dart';
 import 'package:ddd_notes/injection.dart';
+import 'package:ddd_notes/presentation/sign_in/sign_in_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -16,7 +17,7 @@ class AppWidget extends StatelessWidget {
       child: MaterialApp(
         title: 'Notes',
         debugShowCheckedModeBanner: false,
-        home: const Center(),
+        home: SignInPage(),
         theme: ThemeData.light().copyWith(
           primaryColor: Colors.green[800],
           accentColor: Colors.blueAccent,
