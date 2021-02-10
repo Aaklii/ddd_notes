919acdf604e23d2632ff05b3f1bb8d1a0929c7f2
Aakash Solanki <solankish66@gmail.com>
2021-02-10 16:38:59 +0530

    Initial Folder Setup Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:injectable/injectable.dart';
-import 'package:ddd_notes/injection.dart';
-import 'package:ddd_notes/presentation/core/app_widget.dart';
-
-
-void main() {
-  configureInjection(Environment.prod);
-  runApp(AppWidget());
-}
 
