e8b6d6b1d0b413de548c6bacd8360f3671d2dac7
Aakash Solanki <solankish66@gmail.com>
2021-02-11 00:12:40 +0530

    Latest Bloc Code Added

---
+++ lib/domain/core/errors.dart
@@ -0,0 +1,24 @@
+// ********************##********************##********************#
+// ********************##********************##********************#
+// ********************##********************##********************#
+
+import 'package:ddd_notes/domain/core/failures.dart';
+
+// ********************##********************##********************#
+// ********************##********************##********************#
+
+// * Start of UnexpectedValueError
+class UnexpectedValueError extends Error {
+  final ValueFailure valueFailure;
+
+  UnexpectedValueError(this.valueFailure);
+
+  @override
+  String toString() {
+    const explanation =
+        'Encountered a ValueFailure at an unrecoverable point. Terminating.';
+    return Error.safeToString('$explanation Failure was: $valueFailure');
+  }
+}
+
+// ***************************END***************************
