c7988785a2414f06a75e221fd0579e7d83512a5f
Aakash Solanki <solankish66@gmail.com>
2021-02-11 09:50:38 +0530

    Value Validators Added

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -3,6 +3,7 @@
 // ********************##********************##********************#
 import 'package:dartz/dartz.dart';
 import 'package:ddd_notes/domain/core/failures.dart';
+import 'package:kt_dart/kt.dart';
 
 // ********************##********************##********************#
 // ********************##********************##********************#
@@ -30,3 +31,64 @@ Either<ValueFailure<String>, String> validatePassword(String input) {
 }
 
 // ***************************END***************************
+
+// * Start of validateMaxStringLength
+//? Validation need to be done for length of note
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength,
+) {
+  if (input.length <= maxLength) {
+    return right(input);
+  } else {
+    return left(
+      ValueFailure.exceedingLength(failedValue: input, max: maxLength),
+    );
+  }
+}
+
+// ***************************END***************************
+
+// * Start of validateStringNotEmpty
+//? Validation need to be done for note is not empty
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(failedValue: input));
+  }
+}
+
+// ***************************END***************************
+
+// * Start of validateSingleLine
+//? Validation need to be done for a todo_item is a single line
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (input.contains('\n')) {
+    return left(ValueFailure.multiline(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
+
+// ***************************END***************************
+
+// * Start of validateMaxListLength
+//? Validation need to be done for length of todo_list does not exist the defined limit
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
+  KtList<T> input,
+  int maxLength,
+) {
+  if (input.size <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.listTooLong(
+      failedValue: input,
+      max: maxLength,
+    ));
+  }
+}
+
+// ***************************END***************************
+
+
