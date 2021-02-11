35d604f23a241e25e38287146e820173fd5a79ec
Aakash Solanki <solankish66@gmail.com>
2021-02-11 17:25:07 +0530

    ToDo list Updated

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -7,6 +7,10 @@ import 'package:ddd_notes/injection.dart';
 import 'package:ddd_notes/presentation/sign_in/sign_in_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ddd_notes/presentation/routes/router.gr.dart'
+    as app_router;
+import 'package:auto_route/auto_route.dart';
+
 
 // ********************##********************##********************#
 // ********************##********************##********************#
@@ -25,8 +29,7 @@ class AppWidget extends StatelessWidget {
       child: MaterialApp(
         title: 'Notes',
         debugShowCheckedModeBanner: false,
-        // builder: ExtendedNavigator(router: Route.Router()),
-        home: SignInPage(),
+        builder: ExtendedNavigator.builder(router: app_router.Router()),
         theme: ThemeData.light().copyWith(
           primaryColor: Colors.green[800],
           accentColor: Colors.blueAccent,
--- lib/presentation/notes/note_form/widgets/todo_list_widget.dart
+++ lib/presentation/notes/note_form/widgets/todo_list_widget.dart
@@ -1,13 +1,14 @@
 // ********************##********************##********************#
 // ********************##********************##********************#
 // ********************##********************##********************#
-
 import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:kt_dart/collection.dart';
 import 'package:ddd_notes/application/notes/note_form/note_form_bloc.dart';
+import 'package:ddd_notes/domain/notes/value_objects.dart';
 import 'package:ddd_notes/presentation/notes/note_form/misc/todo_item_presentation_classes.dart';
 import 'package:provider/provider.dart';
 import 'package:ddd_notes/presentation/notes/note_form/misc/build_context_x.dart';
@@ -46,6 +47,7 @@ class TodoList extends StatelessWidget {
             itemBuilder: (context, index) {
               return TodoTile(
                 index: index,
+                key: ValueKey(context.formTodos[index].id),
               );
             },
           );
@@ -70,19 +72,77 @@ class TodoTile extends HookWidget {
   Widget build(BuildContext context) {
     final todo =
         context.formTodos.getOrElse(index, (_) => TodoItemPrimitive.empty());
+    final textEditingController = useTextEditingController(text: todo.name);
 
-    return ListTile(
-      leading: Checkbox(
-        value: todo.done,
-        onChanged: (value) {
-          context.formTodos = context.formTodos.map(
-            (listTodo) =>
-                listTodo == todo ? todo.copyWith(done: value) : listTodo,
-          );
-          context
-              .bloc<NoteFormBloc>()
-              .add(NoteFormEvent.todosChanged(context.formTodos));
-        },
+    return Slidable(
+      actionPane: const SlidableDrawerActionPane(),
+      actionExtentRatio: 0.15,
+      secondaryActions: [
+        IconSlideAction(
+          caption: 'Delete',
+          icon: Icons.delete,
+          color: Colors.red,
+          onTap: () {
+            context.formTodos = context.formTodos.minusElement(todo);
+            context
+                .bloc<NoteFormBloc>()
+                .add(NoteFormEvent.todosChanged(context.formTodos));
+          },
+        ),
+      ],
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+        child: ListTile(
+          leading: Checkbox(
+            value: todo.done,
+            onChanged: (value) {
+              context.formTodos = context.formTodos.map(
+                (listTodo) =>
+                    listTodo == todo ? todo.copyWith(done: value) : listTodo,
+              );
+              context
+                  .bloc<NoteFormBloc>()
+                  .add(NoteFormEvent.todosChanged(context.formTodos));
+            },
+          ),
+          title: TextFormField(
+            controller: textEditingController,
+            decoration: InputDecoration(
+              hintText: 'Todo',
+              counterText: '',
+              border: InputBorder.none,
+            ),
+            maxLength: TodoName.maxLength,
+            onChanged: (value) {
+              context.formTodos = context.formTodos.map(
+                (listTodo) =>
+                    listTodo == todo ? todo.copyWith(name: value) : listTodo,
+              );
+              context
+                  .bloc<NoteFormBloc>()
+                  .add(NoteFormEvent.todosChanged(context.formTodos));
+            },
+            validator: (_) {
+              return context.bloc<NoteFormBloc>().state.note.todos.value.fold(
+                    // Failure stemming from the TodoList length should NOT be displayed by the individual TextFormFields
+                    (f) => null,
+                    (todoList) => todoList[index].name.value.fold(
+                          (f) => f.maybeMap(
+                            empty: (_) => 'Cannot be empty',
+                            exceedingLength: (_) => 'Too long',
+                            multiline: (_) => 'Has to be in a single line',
+                            orElse: () => null,
+                          ),
+                          (_) => null,
+                        ),
+                  );
+            },
+          ),
+        ),
       ),
     );
   }
