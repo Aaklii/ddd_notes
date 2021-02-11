d21b910d3b0a29f33324b3c40f3f7dac9605c9fc
Aakash Solanki <solankish66@gmail.com>
2021-02-11 08:47:59 +0530

    Sign UI Created

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -2,25 +2,37 @@
 // ********************##********************##********************#
 // ********************##********************##********************#
 
+import 'package:ddd_notes/application/auth/auth_bloc.dart';
+import 'package:ddd_notes/injection.dart';
+import 'package:ddd_notes/presentation/sign_in/sign_in_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 // ********************##********************##********************#
 // ********************##********************##********************#
 
 // * Start of AppWidget
-
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+          getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Notes',
+        debugShowCheckedModeBanner: false,
+        home: SignInPage(),
+        theme: ThemeData.light().copyWith(
+          primaryColor: Colors.green[800],
+          accentColor: Colors.blueAccent,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
           ),
         ),
       ),
